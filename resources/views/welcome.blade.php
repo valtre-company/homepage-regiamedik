4ab1f8522e4d7a3da65a3c84e94efddd6a368994
valtre-company <valtre.company@gmail.com>
2022-04-11 14:47:22 -0500

    Cambios en meta-tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 
 @extends('layouts.app')
-@section('meta_description', 'Servicios integrales de salud para todo tipo de clientes, Belisario Domínguez 2470A, Centro, 64000 Monterrey, N.L. local 10, (Semillero Obispado). Correo; {{ env('EMAIL') }}; Teléfono: {{ env('PHONE_NUMBER') }}. Copyright {{ Carbon\Carbon::now()->year }} Regia MediK.');
-@section('meta_keywords', 'prueba de antigeno, prueba pcr, analisis clinicos, rayos x');
+@section('meta_description', 'Servicios integrales de salud para todo tipo de clientes, Belisario Domínguez 2470A, Centro, 64000 Monterrey, N.L. local 10, (Semillero Obispado). Correo; {{ env('EMAIL') }}; Teléfono: {{ env('PHONE_NUMBER') }}. Copyright {{ Carbon\Carbon::now()->year }} Regia MediK')
+@section('meta_keywords', 'prueba de antigeno, prueba pcr, analisis clinicos, rayos x')
 @section('content')
 
 <div class="st-content" style="margin-bottom: 1045px;">  

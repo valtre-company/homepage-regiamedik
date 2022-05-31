7273cc72897e232d5c6a55ce9554139c64b5228b
valtre-company <valtre.company@gmail.com>
2022-05-31 13:54:07 -0500

    Cambios en cards

--- resources/views/partials/about-section.blade.php
+++ resources/views/partials/about-section.blade.php
@@ -20,7 +20,7 @@
       {{-- <div class="st-section-heading-subtitle">¡En Regia Medi-K cuidamos de tu salud y la de tu empresa!</div> --}}
       <div class="st-section-heading-subtitle mx-auto">Somos una empresa que ofrece un amplio portafolio de servicios integrales de salud, medicamentos, insumos y equipos de diagnóstico.</div>
       <div class="row mt-5 valores justify-content-center flex-wrap">
-        <div class="col-12 col-md-3 mb-2 mb-md-0">
+        <div class="col-12 col-md-3 mb-2 mb-md-0" style="margin-right: 1rem !important;">
           <div class="flip-card">
             <div class="flip-card-inner">
               <div class="flip-card-front card-blue">
@@ -32,7 +32,7 @@
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-3 mb-2 mb-md-0">
+        <div class="col-12 col-md-3 mb-2 mb-md-0" style="margin-right: 1rem !important;">
           <div class="flip-card">
             <div class="flip-card-inner">
               <div class="flip-card-front card-blue">
@@ -45,8 +45,8 @@
           </div>
         </div>
 
-        <div class="col-12 col-md-3 mb-2 mb-md-0">
-          <div class="flip-card" style="margin-left: 1rem !important;">
+        <div class="col-12 col-md-3 mb-2 mb-md-0" style="margin-right: 1rem !important;">
+          <div class="flip-card">
             <div class="flip-card-inner">
               <div class="flip-card-front card-blue">
                 <h2>Valores Agregados</h2>
